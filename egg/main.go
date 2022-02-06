f5bc8c6bbd8690343110c7b3bd286d907c502e1e
devchoplife <dritedurojaiye@gmail.com>
2022-02-06 21:32:12 +0100

    Add egg timer  size and title

--- egg/main.go
+++ egg/main.go
@@ -1,11 +1,17 @@
 package main
 
-import "gioui.org/app"
+import (
+	"gioui.org/app"
+	"gioui.org/unit"
+)
 
 func main() {
 	go func() {
 		//Create new window
-		w := app.NewWindow()
+		w := app.NewWindow(
+			app.Title("Egg Timer"),
+			app.Size(unit.Dp(400), unit.Dp(600)),
+		)
 
 		//Listen for events in the window
 		for range w.Events() {
