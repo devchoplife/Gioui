afe71b9a94f261a690232ff621b0931fd1d66e6e
devchoplife <dritedurojaiye@gmail.com>
2022-02-22 06:52:27 +0100

    Create function to draw the layout

--- egg/main.go
+++ egg/main.go
@@ -5,8 +5,66 @@ import (
 	"os"
 
 	"gioui.org/app"
+	"gioui.org/font/gofont"
+	"gioui.org/io/system"
+	"gioui.org/layout"
+	"gioui.org/op"
 	"gioui.org/unit"
+	"gioui.org/widget"
+	"gioui.org/widget/material"
 )
+
+type C = layout.Context
+type D = layout.Dimensions
+
+func draw(w *app.Window) error {
+	// ops are the operations from the UI
+	var ops op.Ops
+
+	// startButton is a clickable widget
+	var startButton widget.Clickable
+
+	// the defines the material design style
+	th := material.NewTheme(gofont.Collection())
+
+	//Listen for events in the window
+	for e := range w.Events() {
+		// detect what type of event it is
+		switch e := e.(type) {
+		// this is sent when the application should re-render
+		case system.FrameEvent:
+			gtx := layout.NewContext(&ops, e)
+			// Using the flex layout
+			layout.Flex{
+				// Vertical alignment, top to bottom
+				Axis: layout.Vertical,
+				// Empty space is left at the start i.e. at the top
+				Spacing: layout.SpaceStart,
+			}.Layout(gtx,
+				// We insert tow rigid elements
+				// First, a button
+				layout.Rigid(
+					func(gtx layout.Context) layout.Dimensions {
+						btn := material.Button(th, &startButton, "Start")
+						return btn.Layout(gtx)
+					},
+				),
+
+				// then an empty spacer
+				layout.Rigid(
+					// The height of the spacer is 25 Device independent pixels
+					layout.Spacer{Height: unit.Dp(25)}.Layout,
+				),
+			)
+			e.Frame(gtx.Ops)
+
+			// THis is sent when the application is closed
+		case system.DestroyEvent:
+			return e.Err
+		}
+	}
+	return nil
+}
 
 func main() {
 	go func() {
